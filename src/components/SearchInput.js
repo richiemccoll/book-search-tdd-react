c0d95b577227582ffd511c5edb4a7d1bd9510f2a
Richie McColl <richie.mccoll@hotmail.co.uk>
2020-07-11 11:39:56 +0100

    Add some styles for the form.

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,27 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function SearchInput({ onSubmit }) {
-    const [searchInput, setSearchInput] = useState("");
-    function onChange(e) {
-      setSearchInput(e.target.value);
-    }
-    function handleSubmit(e) {
-      e.preventDefault();
-  
-      if (searchInput) {
-        onSubmit(searchInput);
-      }
-    }
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="book-search">Search for a book</label>
-        <input
-          id="book-search"
-          type="text"
-          value={searchInput}
-          onChange={onChange}
-        />
-      </form>
-    );
+  const [searchInput, setSearchInput] = useState("");
+  function onChange(e) {
+    setSearchInput(e.target.value);
   }
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (searchInput) {
+      onSubmit(searchInput);
+    }
+  }
+
+  return (
+    <form className="rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+      <label
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor="book-search"
+      >
+        Search for a book
+      </label>
+      <input
+        id="book-search"
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        type="text"
+        value={searchInput}
+        onChange={onChange}
+      />
+    </form>
+  );
+}
